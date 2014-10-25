35c43788685b72aaaddeb6494cd46000aefb86c7
ashish2199 <ashish2199@gmail.com>
2014-10-24 18:09:59 -0700

    Created a message, rectangle and oval on menu screen
    Changed to game 2.0
    with the help of graphic object we created a
    string using drawString
    rectangle using drawRect
    and oval using drawOval

--- Ham Blaster/src/javagame/Menu.java
+++ Ham Blaster/src/javagame/Menu.java
@@ -17,8 +17,17 @@ public class Menu extends BasicGameState{
 		
 	}
 	
-	//draw stuff on screen   graphic object is like the paintbrush to draw stuff
+	//draw stuff on screen   
+	//graphic object g is like the paintbrush to draw stuff
 	public void render(GameContainer gc, StateBasedGame sbg,Graphics g) throws SlickException{
+		// to show a message at 50,50
+		g.drawString("Are you ready to blast some Ham!! ??", 50, 50);
+		
+		// draw a rectangle at 50,100  with properties width:60 and height:120
+		g.drawRect(50, 100, 60, 120);
+		
+		// draw oval at 200,130 with width 130 and height 80
+		g.drawOval(200, 130, 130, 80);
 		
 	}
 	
